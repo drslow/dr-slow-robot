01f534a7c5b157340baf953b97f382b3035bd1f9
drslow <aysomaro@gmail.com>
2016-11-30 16:43:52 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {220990589},--Sudo users
+    sudo_users = {204507468,253622411,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
